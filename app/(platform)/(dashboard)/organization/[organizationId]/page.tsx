11273a29f7d308df77b84ed7ff60d056a61c81ae
asaf bar <assafbar81@gmail.com>
2023-12-07 09:45:04 +0200

    authentication with clerk package: 1. use organization feature 2. add logic to middleware 3. add variant to button component

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,3 @@
+export default function organizationPage() {
+  return <div>Organization Page!</div>;
+}
--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,36 @@
+import { NextResponse } from "next/server";
 // TODO: implement custom authentication logic
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // if user logged in and is on a public route, redirect them to the organization page
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    // if user is not logged in and is on a public route, redirect them back to the original route
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    // if user is logged in and is not associated with an organization, redirect them to the organization selection page
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
