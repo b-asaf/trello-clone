3c73d9ccbc02a42e73f84f7c3c603805eaf892aa
asaf bar <assafbar81@gmail.com>
2023-12-13 10:54:09 +0200

    1. list creation implementation 2. DB - add Card & list models 3. DB - create connection between board, list & card

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,3 +1,33 @@
-export default function BoardIdPage() {
-  return <div>Board Id Page</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import ListContainer from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function BoardIdPage({ params }: BoardIdPageProps) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = db.list.findMany({
+    where: { boardId: params.id, board: { orgId } },
+    include: { cards: { orderBy: { order: "asc" } } },
+    orderBy: { order: "asc" },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      {/* @ts-ignore */}
+      <ListContainer boardId={params.id} data={lists} />
+    </div>
+  );
 }
